19c69fd783ee1a881e8a4ddeba066ec5c781f52e
Sungmin Lee <sungminlee417@gmail.com>
2022-06-04 12:00:44 -0700

    finished phase 1

--- server/app.js
+++ server/app.js
@@ -1,13 +1,20 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+require("express-async-errors");
+
+app.use(express.json());
+
+app.use("/static", express.static("assets"));
 
 // For testing purposes, GET /
-app.get('/', (req, res) => {
-  res.json("Express server running. No content provided at root level. Please use another route.");
+app.get("/", (req, res) => {
+  res.json(
+    "Express server running. No content provided at root level. Please use another route."
+  );
 });
 
 // For testing express.json middleware
-app.post('/test-json', (req, res, next) => {
+app.post("/test-json", (req, res, next) => {
   // send the body as JSON with a Content-Type header of "application/json"
   // finishes the response, res.end()
   res.json(req.body);
@@ -15,9 +22,10 @@ app.post('/test-json', (req, res, next) => {
 });
 
 // For testing express-async-errors
-app.get('/test-error', async (req, res) => {
-  throw new Error("Hello World!")
+app.get("/test-error", async (req, res) => {
+  throw new Error("Hello World!");
+  // res.send("Error: Hello World");
 });
 
 const port = 5000;
-app.listen(port, () => console.log('Server is listening on port', port));
+app.listen(port, () => console.log("Server is listening on port", port));
